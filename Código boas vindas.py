28dc753e857e758968a6db2d8b9c486ec76523e9
Benício Macedo Silva <112590998+Benas64@users.noreply.github.com>
2023-02-21 20:02:36 -0300

    treinamento Pandas(21/01/22)

--- Código boas vindas.py
+++ Código boas vindas.py
@@ -1,16 +1,6 @@
-#1.Importar bibliotecas necessárias
-import pywhatkit
-import keyboard
-import time
-from datetime import datetime
+#treinamento de pandas e numpy
+import pandas as pd
+import numpy as np
 
-#2.Definir quais contatos receberão as mensagens
-contatos=['+5586999270061','+5586999270062','+5586988291825']
-#3.Definir intervalos de envio
-while len(contatos)>=1:
-    pywhatkit.sendwhatmsg(contatos[0],'VAMOS AUTOMATIZAR TUDO!',datetime.now().hour,datetime.now().minute+2)
-    del contatos[0]
-    time.sleep(60)
-    keyboard.press_and_release('ctrl+w')
     
 
